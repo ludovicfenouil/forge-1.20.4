eabfa1e9edd0e285ced5e4607f1fa2e0a7da4715
ludovicfenouil <fenouil.ludovic@sfr.fr>
2024-05-19 22:18:08 +0200

    KaupenJoe modding tutorial #2

--- src/main/java/net/ludovicfenouil/tyriacraft/TyriaCraft.java
+++ src/main/java/net/ludovicfenouil/tyriacraft/TyriaCraft.java
@@ -1,6 +1,9 @@
 package net.ludovicfenouil.tyriacraft;
 
 import com.mojang.logging.LogUtils;
+import net.ludovicfenouil.tyriacraft.item.TyriaCraftItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -29,6 +32,9 @@ public class TyriaCraft
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        TyriaCraftItems.register(modEventBus);
+
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
@@ -43,6 +49,9 @@ public class TyriaCraft
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(TyriaCraftItems.SAPPHIRE);
+        }
 
     }
 
---
+++ src/main/java/net/ludovicfenouil/tyriacraft/item/TyriaCraftItems.java
@@ -0,0 +1,20 @@
+package net.ludovicfenouil.tyriacraft.item;
+
+import net.ludovicfenouil.tyriacraft.TyriaCraft;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class TyriaCraftItems {
+    public static final DeferredRegister<Item> ITEMS =
+           DeferredRegister.create(ForgeRegistries.ITEMS, TyriaCraft.MOD_ID);
+
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
